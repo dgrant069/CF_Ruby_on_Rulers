d7822ab385abb8b6882d0198074bee76fdcc3d85
Dylan Grant <dgrant069@hotmail.com>
2013-11-05 15:32:26 -0800

    added rescue to exception for action not existing

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -11,9 +11,15 @@ module Rulers
 
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
-      text = controller.send(act)
-      `echo debug > debug.txt`;
+
+      begin
+        text = controller.send(act)
+      rescue Exception => e
+        text = "Can't display a quote"
+      end
+      `echo debug > debug.txt`; # This doesn't append, it overwrites with "debug", need to update with ... #{debug} >>...
         [200, {'Content-Type' => 'text/html'}, [text]]
+      }
     end
   end
 
