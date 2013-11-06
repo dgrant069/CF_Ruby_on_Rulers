98dae406e19d3e26d680100397ccb0299d370385
Dylan Grant <dgrant069@hotmail.com>
2013-11-06 14:48:53 -0800

    Now has 'db' for 1 quote with controller and view acting accordingly

--- best_quotes/app/controllers/quotes_controller.rb
+++ best_quotes/app/controllers/quotes_controller.rb
@@ -5,6 +5,11 @@ class QuotesController < Rulers::Controller
     render :index
   end
 
+  def quote_1
+    quote_1 = FileModel.find(1)
+    render :quote, :obj => quote_1
+  end
+
   def a_quote
     @verb = "licking"
     @noun = "Izze"
